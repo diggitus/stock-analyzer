8fd484e07968d2313d77af91f97dcf3404bc2ee1
diggitus <diggitus@web.de>
2018-04-01 14:57:42 +0200

    Implemented the view for valuation history

--- src/app/pages/dashboard/dashboard-page.component.ts
+++ src/app/pages/dashboard/dashboard-page.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { FinancialsState, HeaderState, ValuationState } from 'app/app.state';
 import { Valuation } from 'app/model/valuation';
+import { ValuationHistory } from 'app/model/valuationHistory';
 import { FinancialsActions } from 'app/services/financials/financials.actions';
 import { ValuationActions } from 'app/services/valuation/valuation.actions';
 import { Observable } from 'rxjs/Observable';
@@ -25,7 +26,8 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     private valuationStateSubscription: Subscription;
     private financialsStateSubscription: Subscription;
 
-    currentValues: any;
+    valuation: any;
+    valuationHistory: any;
 
     /**
      * Constructor.
@@ -40,7 +42,8 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
      */
     ngOnInit() {
         this.valuationStateSubscription = this.valuationState.subscribe(state => {
-            this.currentValues = this.getCurrentValues(state.valuation);
+            this.valuation = this.getValuationValues(state.valuation);
+            this.valuationHistory = this.getValuationHistoryValues(state.valuationHistory);
         });
 
         this.financialsStateSubscription = this.financialsState.subscribe(state => {
@@ -64,8 +67,9 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     /**
      * Returns an object with current values of the stock.
      * @param valuation The valuation data.
+     * @return object with valuation values.
      */
-    private getCurrentValues(valuation: Valuation): any {
+    private getValuationValues(valuation: Valuation): any {
         return [
             {
                 de: 'KGV',
@@ -88,6 +92,49 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
                 value: valuation.dividendYield
             }
         ];
+    }
+
+    /**
+     * Returns an object with valuation history values.
+     * @param valuationHistory The valuation history values.
+     * @return valuation history object for view.
+     */
+    private getValuationHistoryValues(valuationHistory: ValuationHistory): any {
+        return [
+            {
+                de: 'KGV',
+                en: 'price / earnings',
+                value: this.calcAvg(valuationHistory.priceEarnings)
+            },
+            {
+                de: 'KBV',
+                en: 'price / book',
+                value: this.calcAvg(valuationHistory.priceBook)
+            },
+            {
+                de: 'KUV',
+                en: 'price / sales',
+                value: this.calcAvg(valuationHistory.priceSales)
+            },
+            {
+                de: 'Cash Flow',
+                en: 'price / cash flow',
+                value: this.calcAvg(valuationHistory.priceCashFlow)
+            }
+        ];
+    }
+
+    /**
+     * Calculates the average value of the given list item values.
+     * @param valueList The item list.
+     * @return average value.
+     */
+    private calcAvg(valueList: Array<number> | null): number | null {
+        if (valueList) {
+            const avgValue = valueList.reduce((prevValue: number, curValue: number) => prevValue + curValue) / valueList.length;
+            return Math.round(avgValue * 100) / 100;
+        }
+        return null;
     }
 
 }
